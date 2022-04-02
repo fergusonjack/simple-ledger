c793e224dc1d9be7c07aa3a0f36453bced41df28
Jack Ferguson <jack9283@live.com>
2022-04-02 17:48:03 +0100

    test

--- src/main/java/jack/ledger/LedgerApplication.java
+++ src/main/java/jack/ledger/LedgerApplication.java
@@ -3,9 +3,16 @@ package jack.ledger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * LedgerApplication entry point
+ */
 @SpringBootApplication
 public class LedgerApplication {
 
+	/**
+	 * Main method/entry point
+	 * @param args entry point args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(LedgerApplication.class, args);
 	}
