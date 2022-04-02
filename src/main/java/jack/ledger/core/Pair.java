744fe5fef3e85682d9dee8a0b0d218d74149139d
Jack Ferguson <jack9283@live.com>
2022-04-02 18:44:50 +0100

    test

--- src/main/java/jack/ledger/core/Pair.java
+++ src/main/java/jack/ledger/core/Pair.java
@@ -1,10 +1,22 @@
 package jack.ledger.core;
 
+/**
+ * Simple pair class.
+ *
+ * @param <T> first type.
+ * @param <U> second type.
+ */
 public class Pair<T, U> {
 
     private T first;
     private U second;
 
+    /**
+     * Constructor
+     *
+     * @param first first.
+     * @param second second.
+     */
     public Pair(T first, U second) {
         this.first = first;
         this.second = second;
